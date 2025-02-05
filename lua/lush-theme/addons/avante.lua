6983edb6ccbeeb8eee868022887f08ee1652668a
Saiba Tenpura <61359816+saiba-tenpura@users.noreply.github.com>
2025-02-05 21:54:59 +0100

    Adjust title color to improve contrast and fix trailing spaces.

--- lua/lush-theme/addons/avante.lua
+++ lua/lush-theme/addons/avante.lua
@@ -3,17 +3,17 @@ local colors = require('lush-theme.colors')
 
 local spec = lush(function()
   return {
-    AvanteTitle                 { bg = colors.light_yellow, }, -- Title                                        
-    AvanteReversedTitle         { fg = colors.light_yellow, }, -- Used for rounded border                      
-    AvanteSubtitle              { bg = colors.cyan, }, -- Selected code title                          
-    AvanteReversedSubtitle      { fg = colors.cyan, }, -- Used for rounded border                      
-    AvanteThirdTitle            { bg = colors.light_gray, }, -- Prompt title                                 
-    AvanteReversedThirdTitle    { fg = colors.light_gray, }, -- Used for rounded border                      
-    -- AvanteConflictCurrent       {}, -- Current conflict highlight                   
-    -- AvanteConflictIncoming      {}, -- Incoming conflict highlight                  
-    -- AvanteConflictCurrentLabel  {}, -- Current conflict label highlight             
-    -- AvanteConflictIncomingLabel {}, -- Incoming conflict label highlight            
-    -- AvantePopupHint             {}, -- Usage hints in popup menus                   
+    AvanteTitle                 { fg = colors.background.lighten(25), bg = colors.yellow, gui = 'bold', }, -- Title
+    AvanteReversedTitle         { fg = colors.yellow, }, -- Used for rounded border
+    AvanteSubtitle              { fg = colors.background.lighten(25), bg = colors.cyan, gui = 'bold', }, -- Selected code title
+    AvanteReversedSubtitle      { fg = colors.cyan, }, -- Used for rounded border
+    AvanteThirdTitle            { bg = colors.light_gray, }, -- Prompt title
+    AvanteReversedThirdTitle    { fg = colors.light_gray, }, -- Used for rounded border
+    -- AvanteConflictCurrent       {}, -- Current conflict highlight
+    -- AvanteConflictIncoming      {}, -- Incoming conflict highlight
+    -- AvanteConflictCurrentLabel  {}, -- Current conflict label highlight
+    -- AvanteConflictIncomingLabel {}, -- Incoming conflict label highlight
+    -- AvantePopupHint             {}, -- Usage hints in popup menus
     AvanteInlineHint            { fg = colors.background.lighten(30), }, -- The end-of-line hint displayed in visual mode
   }
 end)
